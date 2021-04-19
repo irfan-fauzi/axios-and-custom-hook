15951749183a7772419b3305d8716e43f6fa23e4
irfan-fauzi <irfanwintech@yahoo.com>
2021-04-19 16:32:54 +0700

    add url

--- src/components/Random_V1.jsx
+++ src/components/Random_V1.jsx
@@ -6,25 +6,26 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 
 const Random_V1 = () => {
   const [gif, setGif] = useState('');
-
+  const fetchGif = async () => {
+    const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
+ 
+    const {data} = await axios.get(url);
+    const imgUrl = data.data.images.downsized.url;
+    setGif(imgUrl)
+  }
   useEffect(() => {
-
-    const fetchGif = async () => {
-      const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
-   
-      const {data} = await axios.get(url);
-      const imgUrl = data.data.images.downsized.url;
-      setGif(imgUrl)
-    }
-
     fetchGif()
-   
   },[])
+
+  const handleClick = () => {
+   fetchGif()
+  }
   return (
-    <>
+    <div className="container">
      <h1>Random</h1>
-     <img src={gif} alt=""/>
-    </>
+     <img src={gif} alt="gif" width="400"/>
+     <button onClick={handleClick} >acak</button>
+    </div>
   )
 }
 
