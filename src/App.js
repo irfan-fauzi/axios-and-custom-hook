cada42407be42e73b228c2a92b7d399d4656dc42
irfan-fauzi <irfanwintech@yahoo.com>
2021-04-19 15:58:15 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import './App.css';
+import './index.css';
+import Random from './components/Random_V1';
+import Tag from './components/Tag_V1';
 
 function App() {
   return (
-    <div className="App">
+    <div>
      <h1>hello world</h1>
-      
+     <div className="main-container">
+        <Random />
+        <Tag />
+     </div>
     </div>
   );
 }
---
+++ src/components/Random_V1.jsx
@@ -0,0 +1,31 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+// GET API KEY from .env
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+const Random_V1 = () => {
+  const [gif, setGif] = useState('');
+
+  useEffect(() => {
+
+    const fetchGif = async () => {
+      const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
+   
+      const {data} = await axios.get(url);
+      const imgUrl = data.data.images.downsized.url;
+      setGif(imgUrl)
+    }
+
+    fetchGif()
+   
+  },[])
+  return (
+    <>
+     <h1>Random</h1>
+     <img src={gif} alt=""/>
+    </>
+  )
+}
+
+export default Random_V1
